10d7ff6b8ec75b189049f67356a2a56b9852c824
Polyxen1a <maslowa@inbox.ru>
2023-07-02 21:25:43 +0300

    Initial commit

---
+++ src/main/java/com/example/functionalinterfacesandlambdaapplicationv3/FunctionalInterfacesAndLambdaApplicationV3Application.java
@@ -0,0 +1,13 @@
+package com.example.functionalinterfacesandlambdaapplicationv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalInterfacesAndLambdaApplicationV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalInterfacesAndLambdaApplicationV3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/functionalinterfacesandlambdaapplicationv3/FunctionalInterfacesAndLambdaApplicationV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.functionalinterfacesandlambdaapplicationv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalInterfacesAndLambdaApplicationV3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
